5d8e7125b4b89a3fb48e2afbaa2a466a7a01d513
tikrack <tikrackcode@gmail.com>
2025-12-12 14:10:07 +0330

    new Version

--- src/worker.js
+++ src/worker.js
@@ -1,13 +1,19 @@
 const TELEGRAM_BOT_TOKEN = '8309141293:AAHtu6KB0cSd7dYzfCW2m5OnngWQyMHbkqQ';
 
+const ALLOWED_USERNAMES = ['tikrack'];
+
 async function sendMessage(chatId, text) {
 	const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
 	const body = {
-		chat_id: chatId, text: text, parse_mode: 'HTML'
+		chat_id: chatId,
+		text: text,
+		parse_mode: 'HTML',
 	};
 
 	return await fetch(url, {
-		method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(body)
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(body),
 	});
 }
 
@@ -17,12 +23,22 @@ export default {
 			return new Response('Not Found', { status: 404 });
 		}
 
-		const res = await request.json();
-		const chatId = res?.message?.chat?.id;
-		const text = res?.message?.text;
+		const {
+			message: {
+				text,
+				from: { username, first_name, last_name },
+				chat: { id, type },
+			},
+		} = await request.json();
 
-		await sendMessage(chatId, JSON.stringify(res));
+		if (type !== "private") {
+			await sendMessage(id, `سلام سلام ${first_name} 😎✨
+می‌دونی که می‌خوای استفاده کنی، ولی 😅 نمی‌تونی بدون حضور من!
+باید بیای شخصی 🤝 تا بتونی بهره‌مند شی 🎉
+نه توی گروه، نه چنل، نه جای دیگه! فقط شخصی 😇🪻
+بخدا شرمندم، ولی اینم راهشه 😉🤗`);
+		}
 
 		return new Response('OK');
-	}
+	},
 };
