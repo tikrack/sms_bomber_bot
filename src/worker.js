02c64994e3b5467df7b5d8ac29168a1299565e86
tikrack <tikrackcode@gmail.com>
2025-12-12 13:37:55 +0330

    new Version

--- src/worker.js
+++ src/worker.js
@@ -1,12 +1,5 @@
 export default {
   async fetch(request, env, ctx) {
-		const response = await env.TikiAi.run(
-			"@cf/meta/llama-3.1-8b-instruct",
-			{
-				prompt: "What is the origin of the phrase Hello, World",
-			}
-		);
-
-    return new Response(JSON.stringify(response));
+    return new Response("Hello");
   }
 };
